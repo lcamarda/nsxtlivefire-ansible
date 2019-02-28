ddb89ea5044a10fbc77e5ecbfaaea63c179abe74
lcamarda <camarda.luca@gmail.com>
2019-02-28 09:13:58 -0500

    Update Ext1_PG_key.ps1

--- Ext1_PG_key.ps1
+++ Ext1_PG_key.ps1
@@ -1,3 +1,4 @@
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false | out-null
 Connect-VIServer vcsa-01a.corp.local -User Administrator@vsphere.local -Password VMware1! | out-null
 $PG = Get-VDPortgroup -name Ext1_PG
 return $PG.key
